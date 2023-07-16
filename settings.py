43bff8d1d27e3799aecbbe35b71e07d733fd27aa
DESKTOP-14B3TFI\FlossMeat <785154148@qq.com>
2023-07-16 10:50:04 +0800

    修改宽度至8000

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
 
         #子弹设置
         self.bullet_speed = 2.0
-        self.bullet_width = 8000
+        self.bullet_width = 10000
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allow = 3 #限制子弹数量
